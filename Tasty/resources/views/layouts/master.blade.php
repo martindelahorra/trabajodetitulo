e36df1e3455ad8adbc7cc0a58347c5614d1315b3
unknown <martindelahorra@gmail.com>
2019-03-14 22:45:30 -0300

    a

--- Tasty/resources/views/layouts/master.blade.php
+++ Tasty/resources/views/layouts/master.blade.php
@@ -21,6 +21,7 @@
                 </div>
             </div>
         </div>
+        
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#"><b>Tasty</b></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
